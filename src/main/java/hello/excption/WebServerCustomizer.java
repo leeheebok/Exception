d15369ec2ec02e420c1092438b892ccdf573a92c
heebok <lehebk@naver.com>
2021-07-17 19:38:43 +0900

    MyHandlerExceptionResolver

--- src/main/java/hello/excption/WebServerCustomizer.java
+++ src/main/java/hello/excption/WebServerCustomizer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
-    @Override
+//    @Override
     public void customize(ConfigurableWebServerFactory factory) {
 
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
